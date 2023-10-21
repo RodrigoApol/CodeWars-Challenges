14523ad43ac74271f3a7437e8bf2eb50db00cd4b
Rodrigo Apolinário <rodrigo_ap2001@outlook.com>
2023-10-20 21:13:14 -0300

    Add MessiGoals Challenge

---
+++ CodeWarsChallenges/Challenges/MessiGoals.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeWarsChallenges.Challenges;
+
+// Messi é um jogador de futebol com gols em três ligas: 
+// LaLiga 
+// Copa del Rey 
+// Champions 
+
+// Complete a função para retornar o número total de gols nas três ligas. 
+// Nota: a entrada sempre será válida. 
+
+// Por exemplo: 5, 10, 2  -->  17
+public class MessiGoals
+{
+    // int totalGoals = 0;
+    // public int GetGoals(int laLiga, int copaDelRey, int champions)
+    // {
+    //     return totalGoals = laLiga + copaDelRey + champions;
+    // }
+    public int GetGoals(int[] totalGoals) => totalGoals.Sum();
+}
